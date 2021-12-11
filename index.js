0a84c88ed47f91c857c6fa1e2085382836f879f4
gstrehlow <90304692+gstrehlow@users.noreply.github.com>
2021-12-11 17:09:14 -0600

    display tables in CLI

---
+++ index.js
@@ -0,0 +1,60 @@
+const inq = require('inquirer');
+const db = require('./db/connection.js');
+
+function firstPrompt(){
+    inq.prompt({
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: ['View All Departments', 'View All Roles', 'View All Employees','Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Exit']
+    })
+    .then((answer)=>{
+        let sql;
+        if (answer.action === 'View All Departments'){
+            sql = "select departments.id as 'ID', departments.name as 'Department' from departments";
+            makeTable(sql, null);
+        }
+        else if (answer.action === 'View All Roles'){
+            sql = "select roles.id as 'ID', roles.title as 'Job Title', roles.salary as 'Salary'," + "departments.name as 'Department' from roles " + "left join departments on roles.department_id = departments.id";
+            makeTable(sql, null);
+        }
+        else if (answer.action === 'View All Employees'){
+            sql = "select s1.id as 'ID', s1.first_name as 'First Name', s1.last_name as 'Last Name'," +
+            "roles.title as 'Job Title', departments.name as 'Department'," +
+            "(CONCAT(s2.first_name,' ',s2.last_name)) as 'Manager Name', roles.salary as 'Salary' " +
+            "from employees s1 " +
+            "left join roles on s1.role_id = roles.id " +
+            "left join employees s2 on s1.manager_id = s2.id " + 
+            "left join departments on roles.department_id = departments.id";
+            makeTable(sql, null);
+        }
+        else if (answer.action === 'Add a Department'){
+            addDepartment();
+        }
+        else if (answer.action === 'Add a Role'){
+            addRole();
+        }
+        else if (answer.action === 'Add an Employee'){
+            addEmployee();
+        }
+        else if (answer.action === 'Update an Employee Role'){
+            updateEmployeeRole();
+        }
+        else if (answer.action === 'Exit'){
+            return console.log('Goodbye!');
+        }
+    })
+}
+
+function makeTable(query, param){
+    db.query(query, param, (err, result) =>{
+        if (err){
+            console.log(err);
+            return firstPrompt();
+        }
+        console.table(result);
+        return firstPrompt();
+    })
+}
+
+firstPrompt();
